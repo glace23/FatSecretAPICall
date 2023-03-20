aefc040e745350422a897946a0761c55b936f9ae
Scott Li <vikash23galce@gmail.com>
2023-03-19 20:28:33 -0600

    Create Authentication models and methods to access FatSecret API

---
+++ FatSecretAPICall/Authentication/FatSecretTokenResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatSecretAPICall.Authentication
+{
+    public class FatSecretTokenResponse
+    {
+        public string Access_Token { get; set; }
+        public string Token_Type { get; set; }
+        public int Expires_In { get; set; }
+    }
+}
---
+++ FatSecretAPICall/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FatSecretAPICall.Authentication;
+
+Console.WriteLine("Hello, World!");
+
+var cred = new FatSecretCredentials
+{
+    ClientKey = "44a3ee4ca84b42ebb3234bc6bf66518c",
+    ClientSecret = "29c8029e8f1b4fdcae11abc7d1babfdd"
+};
+
+var auth = new FatSecretAuthenticationManager(cred);
+Console.WriteLine(await auth.GetAuthHeaderAsync());
+Console.WriteLine();
